b85cd1c26e307976f1cf3e77c0247d2e51a174e7
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-01-26 20:13:32 +0000

    [2] Practice task8 returns a boolean if number is odd.

---
+++ src/main/java/Namu_Darbai_01_04/Task8_OddTest.java
@@ -0,0 +1,22 @@
+package Namu_Darbai_01_04;
+
+
+import java.util.Scanner;
+
+/*
+Write a boolean method called isOdd() in a class called OddTest,
+ which takes an int as input and returns true if the it is odd.
+ */
+public class Task8_OddTest {
+
+    public static boolean isOdd(int number) {
+        return number % 2 != 0;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Iveskite skaiciu ir suzinokite ar jis yra nelyginis: ");
+        System.out.println(Task8_OddTest.isOdd(scan.nextInt()));
+    }
+}

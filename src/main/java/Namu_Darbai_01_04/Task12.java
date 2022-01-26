ed114332524f85d7da4d4f1b97b7b088d3a08e0c
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-01-26 20:18:45 +0000

    [2] started working on task12

---
+++ src/main/java/Namu_Darbai_01_04/Task12.java
@@ -0,0 +1,14 @@
+package Namu_Darbai_01_04;
+
+import java.util.Scanner;
+
+public class Task12 {
+    /*
+    Write a Java program to convert minutes into a number of years and days
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Iveskite koki kieki minuciu norite apdoroti.");
+        int minutes = scan.nextInt();
+    }
+}

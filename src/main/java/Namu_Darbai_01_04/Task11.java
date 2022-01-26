ceaf283ca5f816dacce1732a38eeb83eca7343ff
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-01-26 20:17:43 +0000

    [2] Practice task11 swapping string variables places

---
+++ src/main/java/Namu_Darbai_01_04/Task11.java
@@ -0,0 +1,17 @@
+package Namu_Darbai_01_04;
+
+import java.util.Scanner;
+
+public class Task11 {
+    // Swapping string variables
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Iveskite pasirinkta texta: ");
+        String text = scan.nextLine();
+        /* String firstWord = text.substring(0,text.indexOf(' '));
+         String lastWord = text.substring(text.lastIndexOf(' '));
+        */
+        text = text.substring(text.lastIndexOf(' ')+1) + text.substring(text.indexOf(' '), text.lastIndexOf(' ')+1) + text.substring(0, text.indexOf(' '));
+        System.out.println(text);
+    }
+}

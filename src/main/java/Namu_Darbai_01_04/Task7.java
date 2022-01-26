0bff050c759df382c973a6e24ac31ba5dfebbb6e
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-01-26 20:11:41 +0000

    [2] Practice task7   Scanner input, print line.

---
+++ src/main/java/Namu_Darbai_01_04/Task7.java
@@ -0,0 +1,16 @@
+package Namu_Darbai_01_04;
+
+import java.util.Scanner;
+
+/*
+Write a Java program by using Scanner class  to prompt a user to
+ input his/her name and then the output will be shown as an example below:Hello Dara!
+ */
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please input your name: ");
+        String name = scan.nextLine();
+        System.out.println("Hello " + name);
+    }
+}

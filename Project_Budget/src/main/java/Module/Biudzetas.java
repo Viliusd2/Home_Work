1d6d23cce0e26106b5598c6e43a41da63d517b60
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-02-02 18:56:49 +0000

    [4] started project, starting methods

--- Project_Budget/src/main/java/Module/Biudzetas.java
+++ Project_Budget/src/main/java/Module/Biudzetas.java
@@ -1,13 +1,16 @@
 package Module;
 
+import java.util.Date;
+
 public class Biudzetas {
     private PajamuIrasas[] pajamos = new PajamuIrasas[100];
     private IslaiduIrasas[] islaidos = new IslaiduIrasas[100];
 
     public void pridetiPajamuIrasa(){
-
     }
+
     public void pridetiIslaiduIrasa(){
-
     }
+    public PajamuIrasas[] gautiPajamuIrasa(){return pajamos;}
+    public IslaiduIrasas[] gautiIslaiduIrasa(){return islaidos;}
 }

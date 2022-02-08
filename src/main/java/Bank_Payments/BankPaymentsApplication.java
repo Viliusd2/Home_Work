297051ff20f0b93673a7fa8b42a87a419b0de1b2
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-02-08 18:33:35 +0000

    Added printout as table to bank class.

--- src/main/java/Bank_Payments/BankPaymentsApplication.java
+++ src/main/java/Bank_Payments/BankPaymentsApplication.java
@@ -4,60 +4,69 @@ import Bank_Payments.Bank_Data.Bank;
 import Bank_Payments.Menu.Console.ConsoleMenu;
 import Bank_Payments.Menu.Console.MainMenu;
 
-
 public class BankPaymentsApplication {
 
-    private final ConsoleMenu mainMenu;
-    private final Bank bank = new Bank();
+  private final ConsoleMenu mainMenu;
+  private final Bank bank = new Bank();
 
-    public BankPaymentsApplication() {
-        this.mainMenu = new MainMenu();
+  public BankPaymentsApplication() {
+    this.mainMenu = new MainMenu();
+  }
+
+  public void run() {
+    String selectedOption = mainMenu.printAndRead();
+    // TODO: your implementation here
+    readDataTransfers(selectedOption);
+  }
+
+  public void readDataTransfers(String text) {
+
+    label:
+    for (int i = 0; i < bank.getPayments().size(); i++) {
+      switch (text) {
+        case "1":
+          if (bank.getPayments().get(i).getBank().equalsIgnoreCase("lumi")) {
+            System.out.println(bank.getPayments().get(i));
+          }
+          break;
+        case "2":
+          if (bank.getPayments().get(i).getBank().equalsIgnoreCase("sep")) {
+            System.out.println(bank.getPayments().get(i));
+          }
+          break;
+        case "3":
+          if (bank.getPayments().get(i).getBank().equalsIgnoreCase("shved")) {
+            System.out.println(bank.getPayments().get(i));
+          }
+          break;
+        case "4":
+          printTable();
+          return;
+        case "0":
+          return;
+        default:
+          System.out.println("Wrong input");
+          break label;
+      }
 
     }
 
-    public void run() {
-        String selectedOption = mainMenu.printAndRead();
-        //TODO: your implementation here
-        readDataTransfers(selectedOption);
+    Bank.eilesNr = 0;
+    run();
+  }
 
+  public void printTable() {
+
+    bank.printTableHeader();
+    bank.printSeparatorLine();
+    for (int i = 0; i < bank.getPayments().size() ; i++) {
+      bank.getPayments().get(i).printParameter();
+      if (bank.getPayments().indexOf(bank.getPayments().get(i)) == bank.getPayments().size()-1) {
+        break;
+      } else {
+        bank.printSeparatorLine();
+      }
     }
-
-
-    public void readDataTransfers(String text) {
-
-        label:
-        for (int i = 0; i < bank.getPayments().size(); i++) {
-            switch (text) {
-                case "1":
-                    if (bank.getPayments().get(i).getBank().equalsIgnoreCase("lumi")) {
-                        System.out.println(bank.getPayments().get(i));
-                    }
-                    break;
-                case "2":
-                    if (bank.getPayments().get(i).getBank().equalsIgnoreCase("sep")) {
-                        System.out.println(bank.getPayments().get(i));
-                    }
-                    break;
-                case "3":
-                    if (bank.getPayments().get(i).getBank().equalsIgnoreCase("shved")) {
-                        System.out.println(bank.getPayments().get(i));
-                    }
-                    break;
-                case "4":
-                    System.out.println(bank.getPayments().get(i));
-                    break;
-                case "0":
-                    return;
-                default:
-                    System.out.println("Wrong input");
-                    break label;
-            }
-
-        }
-        Bank.eilesNr = 0;
-        run();
-    }
-
-
-
+    bank.printTableBottom();
+  }
 }
--- src/main/java/Bank_Payments/Bank_Data/Bank.java
+++ src/main/java/Bank_Payments/Bank_Data/Bank.java
@@ -59,7 +59,7 @@ public class Bank implements Methods {
           new LumiData(
               "Lumi",
               LumiData.MOCK_DATA[i][0],
-              LocalDate.parse(LumiData.MOCK_DATA[i][1],format),
+              LocalDate.parse(LumiData.MOCK_DATA[i][1], format),
               LumiData.MOCK_DATA[i][2],
               Double.parseDouble(LumiData.MOCK_DATA[i][3])));
     }
@@ -71,7 +71,7 @@ public class Bank implements Methods {
       payments.add(
           new SepData(
               "Sep",
-                  LocalDate.parse(SepData.MOCK_DATA[i][0],format),
+              LocalDate.parse(SepData.MOCK_DATA[i][0], format),
               SepData.MOCK_DATA[i][1],
               SepData.MOCK_DATA[i][2],
               Double.parseDouble(SepData.MOCK_DATA[i][3])));
@@ -84,7 +84,7 @@ public class Bank implements Methods {
       payments.add(
           new ShvedData(
               "Shved",
-                  LocalDate.parse(ShvedData.MOCK_DATA[i][0], format),
+              LocalDate.parse(ShvedData.MOCK_DATA[i][0], format),
               ShvedData.MOCK_DATA[i][1],
               Double.parseDouble(ShvedData.MOCK_DATA[i][2].replace(',', '.'))));
     }
@@ -110,10 +110,55 @@ public class Bank implements Methods {
     return amount;
   }
 
-
   @Override
   public String toString() {
     eilesNr++;
-    return String.format("Eiles nr: %5d Bank %-8s",eilesNr,getBank());
+    return String.format("Eiles nr: %5d Bank %-8s", eilesNr, getBank());
+  }
+
+  public void printTableHeader() {
+    String topTableBorder =
+        String.format(
+                    "%-12s%-14s%-33s%-23s%-38s%-20s%s",
+                    "\u2554", "\u2566", "\u2566", "\u2566", "\u2566", "\u2566", "\u2557")
+                .replace(' ', '\u2550')
+            + "\n";
+
+    String parameterNameLine =
+        String.format(
+                "\u2551Eiles nr.:%2s Bank %8s Name   %25s Date   %15s IBAN   %30s Amount   %10s",
+                "\u2551", "\u2551", "\u2551", "\u2551", "\u2551", "\u2551")
+            + "\n";
+    System.out.print(topTableBorder + parameterNameLine);
+  }
+
+  public void printParameter() {
+    eilesNr++;
+    String parameterLine =
+        String.format(
+                "\u2551 %-10d\u2551 %-12s\u2551 %-31s\u2551 %-21s\u2551 %-36s\u2551 %-18.2f\u2551",
+                eilesNr, getBank(), getName(), getDate().toString(), getIBAN(), getAmount())
+            + "\n";
+    System.out.print(parameterLine);
+  }
+
+  public void printSeparatorLine() {
+    String separatorLine =
+        String.format(
+                    "%-12s%-14s%-33s%-23s%-38s%-20s%s",
+                    "\u2560", "\u256C", "\u256C", "\u256C", "\u256C", "\u256C", "\u2563")
+                .replace(' ', '\u2550')
+            + "\n";
+    System.out.print(separatorLine);
+  }
+
+  public void printTableBottom() {
+    String botomTableBorder =
+        String.format(
+                    "%-12s%-14s%-33s%-23s%-38s%-20s%s",
+                    "\u255A", "\u2569", "\u2569", "\u2569", "\u2569", "\u2569", "\u255D")
+                .replace(' ', '\u2550')
+            + "\n";
+    System.out.print(botomTableBorder);
   }
 }

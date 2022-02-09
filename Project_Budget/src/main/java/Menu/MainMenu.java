860f51272d4e79fc2efd560063bf5f66e7a8f838
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-02-09 18:50:27 +0000

    WIP Added simple menu String

--- Project_Budget/src/main/java/Menu/MainMenu.java
+++ Project_Budget/src/main/java/Menu/MainMenu.java
@@ -2,10 +2,16 @@ package Menu;
 
 public class MainMenu {
     private static final String mainMenu =
-            String.format(
-                    "%50s"," ").replace(' ','-');
+                    "-----------------------------\n" +
+                            "Main menu for Budget Project\n" +
+                            "Chose option: \n" +
+                            "[1]  - Add Income listing\n" +
+                            "[2]  - Add Spending listing\n" +
+                            "[3]  - Get Income listing\n" +
+                            "[4]  - Get Spending listing\n" +
+                            "[0]  - Exit\n";
 
-  public static void main(String[] args) {
-    System.out.println(mainMenu);
-  }
+    public static void main(String[] args) {
+        System.out.println(mainMenu);
+    }
 }

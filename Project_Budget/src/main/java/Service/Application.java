46a1a40e919536ce61016d17ae708cbe4bf8d491
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-02-11 19:32:00 +0000

    [4] Application class for menu and inputs.

---
+++ Project_Budget/src/main/java/Service/Application.java
@@ -0,0 +1,51 @@
+package Service;
+
+import Modules.Budget;
+import Service.Menu.Console.RecordSettersAndGetters;
+import Service.Menu.Console.ConsoleMenu;
+import Service.Menu.MainMenu;
+
+public class Application {
+  private final ConsoleMenu mainMenu;
+  private final Budget budget = new Budget();
+
+  public Application(){
+    this.mainMenu = new MainMenu();
+  }
+  public void run() {
+    String selectedOption = mainMenu.printAndRead();
+    // TODO: your implementation here
+    choseMenuOption(selectedOption);
+  }
+  public void choseMenuOption(String text) {
+
+    while (!"0".equals(text)){ //"[0]  - Exit\n";
+      switch (text) {
+        case "1":
+//"[1]  - Add Income listing\n"
+          budget.addMoneyEarnedRecord(RecordSettersAndGetters.earningsSetter());
+          break;
+        case "2":
+//"[2]  - Add Spending listing\n"
+          budget.addMoneySpentRecord(RecordSettersAndGetters.spendingsSetter());
+          break;
+        case "3":
+// "[3]  - Get Income listing\n" +
+          budget.getEarningsRecord();
+          break;
+        case "4":
+// "[4]  - Get Spending listing\n" +
+          budget.getSpendingRecord();
+          break;
+        default:
+          System.out.println("Wrong input");
+
+      }
+      text = mainMenu.printAndRead();
+    }
+
+
+  }
+
+
+}

b33a31467111b0bc2bba434f6503aa2b89bdec9d
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-05-05 20:30:14 +0000

    1.6 Added languages with messages.properties for ENG DE LT (class work)

---
+++ Ra-Dom/eshop_ra_dom/src/main/resources/static/js/language.js
@@ -0,0 +1,10 @@
+$(document).ready(function ()
+    {
+        $("#languageSelector").change(function () {
+            let selectedOption = $('#languageSelector').val();
+            if (selectedOption !== ''){
+                window.location.replace('?lang=' + selectedOption);
+            }
+
+        });
+    })

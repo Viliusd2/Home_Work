d395af7050fab49ccb4ecac557ffd03c228ece3f
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-05-05 20:31:25 +0000

    1.6 database table schema and initial data files (class work)

---
+++ Ra-Dom/eshop_ra_dom/src/main/resources/db/initial-data.sql
@@ -0,0 +1,15 @@
+INSERT INTO PRODUCT (product_id, name, quantity_in_stock, price, flavor)
+VALUES
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991341', 'Cake', 1, 6000.00, 'Strawberry filled with vanilla mousse flavor'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991342', 'Brownie', 1, 5000.00, 'Chocolate brownie with gnash'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991343', 'Brownie', 1, 5000.00, 'Chocolate brownie with cacao cream'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991344', 'Cake', 1, 6000.00, 'Pistachio and raspberry filed with vanilla flavor'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991345', 'Dessert', 3, 2400.00, 'Strawberry filled with vanilla mousse flavor'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991346', 'Dessert', 3, 2400.00, 'Cherry filed with mascarpone vanilla mousse flavor'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991347', 'Dessert', 3, 2400.00, 'Cherry filed with chocolate mousse flavor'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991348', 'Muffin', 6, 3000.00, 'Chocolate'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991358', 'Muffin', 6, 3000.00, 'Vanilla'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991368', 'K-pop', 5, 1500.00, 'Dark beer filling'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991378', 'K-pop', 5, 1500.00, 'Red velvet filling'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991388', 'Cupcakes', 20, 2000.00, 'Chocolate'),
+    ('e4dbc123-a7c2-4bee-a519-e1b9ba991349', 'Cupcakes', 20, 2000.00, 'Vanilla');

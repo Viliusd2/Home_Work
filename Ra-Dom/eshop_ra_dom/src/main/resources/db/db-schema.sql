0b29ab05cfa863548e3c06de6653837cbd1a4d82
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-05-16 21:44:41 +0000

    1.9 expanded db schema and inset sql files

--- Ra-Dom/eshop_ra_dom/src/main/resources/db/db-schema.sql
+++ Ra-Dom/eshop_ra_dom/src/main/resources/db/db-schema.sql
@@ -1,10 +1,27 @@
+DROP TABLE IF EXISTS product_product_categories;
+
 DROP TABLE IF EXISTS PRODUCT;
 
-CREATE TABLE PRODUCT (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    product_id UUID NOT NULL,
-    name VARCHAR(20) NOT NULL,
-    quantity_in_stock INT NOT NULL,
-    price DECIMAL(20, 2) NOT NULL,
-    flavor VARCHAR(500) DEFAULT NULL
+CREATE TABLE PRODUCT
+(
+    id                BIGINT PRIMARY KEY AUTO_INCREMENT,
+    product_id        UUID           NOT NULL,
+    name              VARCHAR(20)    NOT NULL,
+    quantity_in_stock INT            NOT NULL,
+    price             DECIMAL(20, 2) NOT NULL,
+    flavor            VARCHAR(500) DEFAULT NULL
+);
+DROP TABLE IF EXISTS product_category;
+CREATE TABLE product_category
+(
+    id   BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE product_product_categories
+(
+    products_id           BIGINT,
+    product_categories_id BIGINT,
+    FOREIGN KEY (products_id) REFERENCES product (id),
+    FOREIGN KEY (product_categories_id) REFERENCES product_category (id)
 );
--- Ra-Dom/eshop_ra_dom/src/main/resources/db/initial-data.sql
+++ Ra-Dom/eshop_ra_dom/src/main/resources/db/initial-data.sql
@@ -1,15 +1,39 @@
 INSERT INTO PRODUCT (product_id, name, quantity_in_stock, price, flavor)
-VALUES
-    ('e4dbc123-a7c2-4bee-a519-e1b9ba991341', 'Cake', 1, 6000.00, 'Strawberry filled with vanilla mousse flavor'),
-    ('e4dbc123-a7c2-4bee-a519-e1b9ba991342', 'Brownie', 1, 5000.00, 'Chocolate brownie with gnash'),
-    ('e4dbc123-a7c2-4bee-a519-e1b9ba991343', 'Brownie', 1, 5000.00, 'Chocolate brownie with cacao cream'),
-    ('e4dbc123-a7c2-4bee-a519-e1b9ba991344', 'Cake', 1, 6000.00, 'Pistachio and raspberry filed with vanilla flavor'),
-    ('e4dbc123-a7c2-4bee-a519-e1b9ba991345', 'Dessert', 3, 2400.00, 'Strawberry filled with vanilla mousse flavor'),
-    ('e4dbc123-a7c2-4bee-a519-e1b9ba991346', 'Dessert', 3, 2400.00, 'Cherry filed with mascarpone vanilla mousse flavor'),
-    ('e4dbc123-a7c2-4bee-a519-e1b9ba991347', 'Dessert', 3, 2400.00, 'Cherry filed with chocolate mousse flavor'),
-    ('e4dbc123-a7c2-4bee-a519-e1b9ba991348', 'Muffin', 6, 3000.00, 'Chocolate'),
-    ('e4dbc123-a7c2-4bee-a519-e1b9ba991358', 'Muffin', 6, 3000.00, 'Vanilla'),
-    ('e4dbc123-a7c2-4bee-a519-e1b9ba991368', 'K-pop', 5, 1500.00, 'Dark beer filling'),
-    ('e4dbc123-a7c2-4bee-a519-e1b9ba991378', 'K-pop', 5, 1500.00, 'Red velvet filling'),
-    ('e4dbc123-a7c2-4bee-a519-e1b9ba991388', 'Cupcakes', 20, 2000.00, 'Chocolate'),
-    ('e4dbc123-a7c2-4bee-a519-e1b9ba991349', 'Cupcakes', 20, 2000.00, 'Vanilla');
+VALUES ('e4dbc123-a7c2-4bee-a519-e1b9ba991341', 'Vanilla mousse cake', 1, 6000.00, 'Strawberry filled with vanilla mousse flavor'),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991342', 'Chocolate brownie', 1, 5000.00, 'Chocolate brownie with gnash'),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991343', 'Chocolate brownie', 1, 5000.00, 'Chocolate brownie with mascarpone cream'),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991344', 'Vanilla mousse cake', 1, 6000.00,
+        'Pistachio and raspberry filed with vanilla flavor'),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991345', 'Mousse Dessert', 3, 2400.00, 'Strawberry filled with vanilla mousse flavor'),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991346', 'Mousse Dessert', 3, 2400.00,
+        'Cherry filed with mascarpone vanilla mousse flavor'),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991347', 'Mousse Dessert', 3, 2400.00, 'Cherry filed with chocolate mousse flavor'),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991348', 'Muffin Chocolate', 6, 3000.00, 'Chocolate'),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991358', 'Muffin Vanilla', 6, 3000.00, 'Vanilla'),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991368', 'K-pop Dark', 5, 1500.00, 'Dark beer filling'),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991378', 'K-pop Red', 5, 1500.00, 'Red velvet filling'),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991388', 'Cupcakes Chocolate', 20, 2000.00, 'Chocolate'),
+       ('e4dbc123-a7c2-4bee-a519-e1b9ba991349', 'Cupcakes Vanilla', 20, 2000.00, 'Vanilla');
+
+INSERT INTO product_category(name)
+VALUES ('Cakes'),
+       ('Deserts'),
+       ('Cupcakes'),
+       ('Pies'),
+       ('Other');
+
+INSERT INTO product_product_categories(products_id, product_categories_id)
+VALUES (1, 1),
+       (2, 4),
+       (3, 4),
+       (4, 4),
+       (5, 1),
+       (6, 2),
+       (7, 2),
+       (8, 3),
+       (9, 3),
+       (10, 5),
+       (11, 5),
+       (12, 3),
+       (13, 3);
+

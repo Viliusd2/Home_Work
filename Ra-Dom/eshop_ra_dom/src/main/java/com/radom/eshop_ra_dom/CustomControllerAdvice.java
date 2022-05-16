af883275581f3febe4838677659f325443ad9671
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-05-16 21:43:59 +0000

    1.9 init binder for trimming input fields

--- Ra-Dom/eshop_ra_dom/src/main/java/com/radom/eshop_ra_dom/CustomControllerAdvice.java
+++ Ra-Dom/eshop_ra_dom/src/main/java/com/radom/eshop_ra_dom/CustomControllerAdvice.java
@@ -1,11 +1,15 @@
 package com.radom.eshop_ra_dom;
 
-import com.radom.eshop_ra_dom.exception.ProductNotFoundException;
+import com.radom.eshop_ra_dom.product.exception.ProductNotFoundException;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.beans.PropertyEditor;
 import java.util.Date;
 
 
@@ -13,13 +17,19 @@ import java.util.Date;
 public class CustomControllerAdvice {
 
     @ExceptionHandler(ProductNotFoundException.class)
-    public String productNotFoundException(ProductNotFoundException e, Model model){
+    public String productNotFound(ProductNotFoundException e, Model model) {
         model.addAttribute("productId", e.getProductId());
         return "product/error/productNotFound";
     }
+
     @ModelAttribute("modelDate")
     public Date now() {
         return new Date();
     }
 
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        PropertyEditor stringTrimmer = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmer);
+    }
 }

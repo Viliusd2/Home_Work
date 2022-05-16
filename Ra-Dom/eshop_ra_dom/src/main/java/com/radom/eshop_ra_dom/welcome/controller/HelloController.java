8ca9d8d939fb6b227e1cec2a26da253c2e22261b
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-05-16 21:45:32 +0000

    1.9 refactor hello page

--- Ra-Dom/eshop_ra_dom/src/main/java/com/radom/eshop_ra_dom/welcome/controller/HelloController.java
+++ Ra-Dom/eshop_ra_dom/src/main/java/com/radom/eshop_ra_dom/welcome/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
     @GetMapping("/")
     public String hello(){
-        return "hello";
+        return "welcome/hello";
     }
 }

8ac86412275688efe84578d7beab67b3c9a29af6
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-05-16 21:49:13 +0000

    2.0 product not found error page and handling

--- Ra-Dom/eshop_ra_dom/src/main/java/com/radom/eshop_ra_dom/product/exception/ProductNotFoundException.java
+++ Ra-Dom/eshop_ra_dom/src/main/java/com/radom/eshop_ra_dom/product/exception/ProductNotFoundException.java
@@ -2,8 +2,6 @@ package com.radom.eshop_ra_dom.product.exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.UUID;
 

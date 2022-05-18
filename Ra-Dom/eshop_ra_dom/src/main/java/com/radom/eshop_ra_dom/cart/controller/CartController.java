ae078ea0a3f67c8ad86453b1263e4a58d726d6fd
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-05-18 16:22:20 +0000

    2.1 post mapping for cart order function on button

--- Ra-Dom/eshop_ra_dom/src/main/java/com/radom/eshop_ra_dom/cart/controller/CartController.java
+++ Ra-Dom/eshop_ra_dom/src/main/java/com/radom/eshop_ra_dom/cart/controller/CartController.java
@@ -2,11 +2,12 @@ package com.radom.eshop_ra_dom.cart.controller;
 
 import com.radom.eshop_ra_dom.cart.dto.CartDto;
 import com.radom.eshop_ra_dom.cart.service.CartService;
-import com.radom.eshop_ra_dom.product.dto.ProductDto;
-import com.radom.eshop_ra_dom.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import java.util.UUID;
 
 @Controller
@@ -31,6 +32,16 @@ public class CartController {
     @PostMapping("/{productId}")
     public String addToCart(@PathVariable UUID productId, @ModelAttribute("cartSession") CartDto cart) {
         cartService.addToCartByProductId(productId,cart);
+
+        return "redirect:/products/list";
+    }
+    @PostMapping
+    public String order(SessionStatus sessionStatus, RedirectAttributes redirectAttributes){
+        //TODO: save into db or do other things to cart data
+
+        sessionStatus.setComplete();
+
+        redirectAttributes.addFlashAttribute("successMessage", "cart.order.message.success");
 
         return "redirect:/products/list";
     }

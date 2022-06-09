ad873cca1e8b95b532dce0439c539e4997ea89d3
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-06-09 15:35:58 +0000

    9. refactor and language properties fix

--- Ra-Dom/eshop_ra_dom/src/main/java/com/radom/eshop_ra_dom/welcome/controller/WelcomePageController.java
+++ Ra-Dom/eshop_ra_dom/src/main/java/com/radom/eshop_ra_dom/welcome/controller/WelcomePageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import static com.radom.eshop_ra_dom.EshopEndpoints.PUBLIC_WORKSPACE;
 
 @Controller
-public class HelloController {
+public class WelcomePageController {
     @GetMapping(PUBLIC_WORKSPACE+"/")
     public String hello(){
         return "welcome/hello";
--- Ra-Dom/eshop_ra_dom/src/test/java/com/radom/eshop_ra_dom/helperTest/MessageServiceTest.java
+++ Ra-Dom/eshop_ra_dom/src/test/java/com/radom/eshop_ra_dom/helperTest/MessageServiceTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-
+//Message test example from Pavel
 @ExtendWith(MockitoExtension.class)
 public class MessageServiceTest {
 

3ea3cac5753bf98a7b7fce112eba1932004d0227
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-06-21 18:04:42 +0000

    [API]11. Split project into modules for restfull application

--- Ra-Dom/eshop_ra_dom/src/main/java/com/radom/eshop_ra_dom/cart/repository/CartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.radom.eshop_ra_dom.cart.repository;
-
-import com.radom.eshop_ra_dom.cart.entity.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
--- Ra-Dom/eshop_ra_dom/src/main/java/com/radom/eshop_ra_dom/product/repository/ProductCategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.radom.eshop_ra_dom.product.repository;
-
-import com.radom.eshop_ra_dom.product.entity.ProductCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
-}

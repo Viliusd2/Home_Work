eda45011a13fb77bf571cdcd739e8ce1f23da4d2
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-06-24 22:06:33 +0000

    12. CRUD OPERATION AND ENDPOINTS FOR CART

--- Ra-Dom/eshop_ra_dom/eshop-api/src/main/java/org/example/eshop/api/cart/controller/CartApiController.java
+++ Ra-Dom/eshop_ra_dom/eshop-api/src/main/java/org/example/eshop/api/cart/controller/CartApiController.java
@@ -1,49 +1,67 @@
 package org.example.eshop.api.cart.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.eshop.api.cart.dto.CartResponse;
 import org.example.eshop.cart.dto.CartDto;
 import org.example.eshop.cart.service.CartService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+
+import javax.validation.Valid;
 import java.security.Principal;
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 public class CartApiController implements CartApiSpec {
     private final CartService cartService;
-
-    public ResponseEntity<Void> addToCart(@PathVariable UUID productId, @ModelAttribute("cartSession") CartDto cart) {
+    public ResponseEntity<Void> addProductToCart(@Valid @RequestBody CartDto cart, @PathVariable UUID productId) {
         if (cartService.addToCartByProductId(productId, cart)) {
-            return ResponseEntity.status(HttpStatus.OK).build();
+            return ResponseEntity.status(HttpStatus.ACCEPTED).build();
         }
 
         return ResponseEntity.notFound().build();
     }
 
-    public ResponseEntity<Void> orderSave(
-            SessionStatus sessionStatus,
-            RedirectAttributes redirectAttributes,
-            @ModelAttribute("cartSession") CartDto cart,
-            Principal principal) {
+    public ResponseEntity<Void> removeProductFromCart(@Valid @RequestBody CartDto cart,@PathVariable UUID productId) {
+        if (cartService.removeFromCartByProductId(productId,cart)){
+            return ResponseEntity.status(HttpStatus.ACCEPTED).build();
+        }
+        return  ResponseEntity.notFound().build();
+    }
+
+    public ResponseEntity<Void> createCart(@Valid @RequestBody CartDto cart, Principal principal) {
         cart.setUserEmail(principal.getName());
         cartService.saveCart(cart);
-        sessionStatus.setComplete();
         if (cartService.findCartByUUID(cart.getCartId())) {
-            redirectAttributes.addFlashAttribute(
-                    "successMessage",
-                    "cart.order.message.success");
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    public CartResponse getCarts() {
+        return CartResponse.builder()
+                .carts(cartService.findAll())
+                .build();
+    }
+
+
+    public ResponseEntity<Void> deleteCart(UUID cartId) {
+        if (cartService.deleteCart(cartId)){
             return ResponseEntity.status(HttpStatus.OK).build();
         }
         return ResponseEntity.notFound().build();
     }
 
+
+    public CartResponse getCartsByUsername(String username) {
+        return CartResponse.builder()
+                .carts(cartService.findCartByUsername(username))
+                .build();
+
+    }
 
 
 }

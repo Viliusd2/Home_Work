5fc1869dd7592f29e4346394ff971fff76e8daa2
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-06-26 15:37:09 +0000

    13. File Controller and file Service working(TODO Need Swagger Documentation)

--- Ra-Dom/eshop_ra_dom/eshop-api/src/main/java/org/example/eshop/api/file/FileController.java
+++ Ra-Dom/eshop_ra_dom/eshop-api/src/main/java/org/example/eshop/api/file/FileController.java
@@ -1,6 +1,10 @@
 package org.example.eshop.api.file;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +17,22 @@ public class FileController {
     private final FileService fileService;
 
     @PostMapping("/api/file/upload")
-    public void saveFile(@RequestParam MultipartFile file){
-        fileService.saveFile(file);
+    public FileResponce saveFile(@RequestParam MultipartFile file) {
+        return fileService.saveFile(file);
+    }
+    @GetMapping("/api/file/download")
+    public ResponseEntity<Resource> getFileByFileName(@RequestParam String fileName) {
+        return ResponseEntity.ok()
+                .headers(getContentDispositionHttpHeader(fileName))
+                .contentType(fileService.getFileMediaTypeByFileName(fileName))
+                .body(fileService.getFile(fileName));
+    }
+
+    private HttpHeaders getContentDispositionHttpHeader(String fileName) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+
+        return headers;
     }
 
 }
--- Ra-Dom/eshop_ra_dom/eshop-api/src/main/java/org/example/eshop/api/file/FileService.java
+++ Ra-Dom/eshop_ra_dom/eshop-api/src/main/java/org/example/eshop/api/file/FileService.java
@@ -1,34 +1,57 @@
 package org.example.eshop.api.file;
 
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.eshop.jpa.file.entity.File;
+import org.example.eshop.jpa.file.repository.FileRepository;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.util.Objects;
+
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class FileService {
 
     private final Path fileLocation = Path.of("./pictures").toAbsolutePath().normalize();
+    private final FileRepository fileRepository;
 
-    public void saveFile(MultipartFile file) {
+    public FileResponce saveFile(MultipartFile file) {
         createDirectory();
         try {
-            Path filePathWithFileName = fileLocation.resolve(Objects.requireNonNull(file.getOriginalFilename()));
+            String[] splitFile = file.getOriginalFilename().split("\\.");
+
+            File savedFileInDb = fileRepository.save(
+                    File.builder()
+                            .fileName(splitFile[0])
+                            .fileExtension(splitFile[1])
+                            .size(file.getSize())
+                            .mediaType(file.getContentType())
+                            .build());
+
+            Path filePathWithFileName = fileLocation.resolve(savedFileInDb.getUniqFileName());
 
             Files.copy(file.getInputStream(), filePathWithFileName, StandardCopyOption.REPLACE_EXISTING);
+            return FileResponce.builder()
+                    .originalFileName(savedFileInDb.getUniqFileName())
+                    .build();
 
-        }catch (IOException e) {
-        log.error("Cannot create File");
-        e.printStackTrace();
-    }
-
-
-
+        } catch (IOException e) {
+            log.error("Cannot create File");
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private void createDirectory() {
@@ -40,6 +63,23 @@ public class FileService {
             log.error("Cannot create directory");
             e.printStackTrace();
         }
+    }
+
+    public Resource getFile(String fileName) {
+        try {
+            InputStream inputStream = Files.newInputStream(fileLocation.resolve(fileName));
+
+            return new InputStreamResource(inputStream);
+        } catch (IOException e) {
+            log.error("Cannot get/create file by file name", e);
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public MediaType getFileMediaTypeByFileName(String fileName) {
+        return MediaType.valueOf(URLConnection.guessContentTypeFromName(fileName));
     }
 
 }

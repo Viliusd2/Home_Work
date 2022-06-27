eead7028f27c65881b99f8221a2003eb5682307f
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-06-27 15:34:17 +0000

    1. First init upload example js files and project init files.

--- Ra-Dom/ui/src/App.js
+++ Ra-Dom/ui/src/App.js
@@ -1,31 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import Example from "./components/MyAwesomeComponent";
-import ButtonWithText from "./components/ButtonWithText";
-import ProductForm from "./components/ProductV2";
-import Product from "./components/Product";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Example name="nepaspaustas"/>
-        <ButtonWithText text="Nebuvo paspaustas"/>
-        <ProductForm />
-        <Product />
-      </header>
+    <div >
+      hello , Vilius
     </div>
   );
 }
---
+++ Ra-Dom/ui/src/components/Product.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+
+function ExampleForProduct() {
+    // Declare multiple state variables!
+    const [product, setProduct] = useState({
+        name: "",
+        price: "",
+        amount: "",
+        description: ""
+    });
+
+    const handleChange = e => {
+
+        setProduct(
+            {
+                ...product,
+                [e.target.name]: e.target.value
+            });
+    }
+
+    function handleSubmit(e) {
+        console.log(product);
+        e.preventDefault();
+    }
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+
+            <input value={product.name} type="text" onChange={handleChange} name="name"/>
+
+            <input value={product.price} type="number" onChange={handleChange} name="price"/>
+
+            <input value={product.amount} type="number" onChange={handleChange} name="amount"/>
+
+            <input value={product.description} type="text" onChange={handleChange} name="description"/>
+
+            <button type="submit" >Submit</button>
+        </form>
+        </>
+    );
+
+}
+export default ExampleForProduct

47b33098087fa371f823612bc4dac870740d45f6
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-06-27 19:45:17 +0000

    [REACT]3. Update some Js files add Routing package and icons to project.

--- Ra-Dom/ui/src/containers/App.js
+++ Ra-Dom/ui/src/containers/App.js
@@ -1,14 +1,29 @@
 import HeaderContainer from './Header/HeaderContainer';
-import ContentContainer from './Content/ContentContainer';
+import {
+    Routes,
+    Route} from "react-router-dom";
 import FooterContainer from './Footer/FooterContainer';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import HomePage from "./Pages/HomePage";
+import ProductsPage from "./Pages/ProductsPage";
+import NewProductPage from './Pages/NewProductPage';
+
 
 function App() {
     return (
-        <div>
-            <HeaderContainer />
-            <ContentContainer />
-            <FooterContainer />
-        </div>
+        <BrowserRouter>
+            <div className='mainApp'>
+                <HeaderContainer/>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/products" element={<ProductsPage />} />
+                    <Route path='/product/create' element={ <NewProductPage /> } />
+                </Routes>
+                <FooterContainer/>
+            </div>
+    </BrowserRouter>
     );
 }
 
--- Ra-Dom/ui/src/containers/Header/HeaderContainer.js
+++ Ra-Dom/ui/src/containers/Header/HeaderContainer.js
@@ -1,42 +1,46 @@
-import {Button, Container, Form, FormControl, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {
+    Button,
+    Container,
+    Form,
+    FormControl,
+    Nav,
+    Navbar,
+    NavDropdown,
+} from 'react-bootstrap';
+import {NavLink} from "react-router-dom";
 
-const HeaderContainer = () =>
-    <Navbar bg="light" expand="lg">
-    <Container fluid>
-        <Navbar.Brand href="#">Eshop Ra-Dom</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-            <Nav
-                className="me-auto my-2 my-lg-0"
-                style={{ maxHeight: '100px' }}
-                navbarScroll
-            >
-                <Nav.Link href="#action1">Product</Nav.Link>
-                <Nav.Link href="#action2">Product List</Nav.Link>
-                <NavDropdown title="Link" id="navbarScrollingDropdown">
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                        Something else here
-                    </NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link href="#" disabled>
-                    Registration
-                </Nav.Link>
-            </Nav>
-            <Form className="d-flex">
-                <FormControl
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-            </Form>
-            <Nav.Link href="#action1">LOGIN</Nav.Link>
-        </Navbar.Collapse>
-    </Container>
-</Navbar>;
+const HeaderContainer = () => (
+    <Navbar bg="light " expand="lg">
+        <Container fluid>
+            <Navbar.Brand to="/" as={NavLink}>Eshop Ra-Dom</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+                <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll
+                >
+                    <Nav.Link to="/product/create" as={NavLink}>Create Product</Nav.Link>
+                    <Nav.Link to="/products" as={NavLink}>Product List</Nav.Link>
+                    <Nav.Link href="#" disabled>Registration</Nav.Link>
+                    <NavDropdown title="Language" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Lietuviu</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">English</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Form className="d-flex">
+                    <FormControl
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                    />
+                    <Button variant="outline-success">Search</Button>
+                </Form>
+                <Nav.Link href="#action1">LOGIN</Nav.Link>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+);
 
 export default HeaderContainer;

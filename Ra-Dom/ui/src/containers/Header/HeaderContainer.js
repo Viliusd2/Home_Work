0e8f8e701415b3fbef85f2a1e628a5528b137137
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-06-27 17:33:08 +0000

    2. Simple Navbar added to HeaderContainer.js

--- Ra-Dom/ui/src/containers/Header/HeaderContainer.js
+++ Ra-Dom/ui/src/containers/Header/HeaderContainer.js
@@ -1,3 +1,42 @@
-const HeaderContainer = () => <h1>Header</h1>;
+import {Button, Container, Form, FormControl, Nav, Navbar, NavDropdown} from "react-bootstrap";
+
+const HeaderContainer = () =>
+    <Navbar bg="light" expand="lg">
+    <Container fluid>
+        <Navbar.Brand href="#">Eshop Ra-Dom</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+            <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll
+            >
+                <Nav.Link href="#action1">Product</Nav.Link>
+                <Nav.Link href="#action2">Product List</Nav.Link>
+                <NavDropdown title="Link" id="navbarScrollingDropdown">
+                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action5">
+                        Something else here
+                    </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#" disabled>
+                    Registration
+                </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+                <FormControl
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+            </Form>
+            <Nav.Link href="#action1">LOGIN</Nav.Link>
+        </Navbar.Collapse>
+    </Container>
+</Navbar>;
 
 export default HeaderContainer;

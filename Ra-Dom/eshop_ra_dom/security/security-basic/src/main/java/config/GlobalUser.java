95f82928b868706aee63ded580a2040f855a049e
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-07-10 10:53:16 +0000

    [API] Basic security config and global users for authentification

---
+++ Ra-Dom/eshop_ra_dom/security/security-basic/src/main/java/config/GlobalUser.java
@@ -0,0 +1,13 @@
+package config;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GlobalUser {
+
+    private String username;
+    private String password;
+    private String[] roles;
+}

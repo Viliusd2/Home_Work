bb0f92d169bcbdf9d2fc06a5af953a8773d55804
Viliusd2 <88612620+Viliusd2@users.noreply.github.com>
2022-07-16 12:48:04 +0000

    [REACT]7. Use Formik on login page with simple validation

--- Ra-Dom/ui/src/containers/Pages/Login/Login.js
+++ Ra-Dom/ui/src/containers/Pages/Login/Login.js
@@ -1,57 +1,75 @@
 import './statics/loginStyle.css'
-import {useState} from "react";
+import {Form, Formik} from "formik";
 
 const Login = () => {
 
-    const [login, setLogin] = useState({
-        email: '',
-        password: ''
-    })
+    const validate = (login) => {
 
-    const onSubmit = (e) => {
-        e.preventDefault()
+        const errors = {}
 
-        console.log("login", login)
-    }
+        if (!login.email.includes("@")) {
+            errors.email = "CIA NE EMAIL'AS!!!";
+        }
 
-    const onChange = (e) => {
-        setLogin({
-            email: e.target.value,
-            password: e.target.value
-        })
+        if (login.password.length < 6 ) {
+            errors.password = "Slaptazodzio ilgis turi buti >= 6"
+        }
+
+        return errors
     }
 
     return (
-        <div className="Auth-form-container">
-            <form className="Auth-form" onSubmit={onSubmit}>
-                <div className="Auth-form-content">
-                    <h3 className="Auth-form-title">Sign In</h3>
-                    <div className="form-group mt-3">
-                        <label>Email address</label>
-                        <input type="email"
-                               className="form-control mt-1"
-                               placeholder="Enter email"
-                               value={login.email}
-                               onChange={onChange}
-                        />
-                    </div>
-                    <div className="form-group mt-3">
-                        <label>Password</label>
-                        <input type="password"
-                               className="form-control mt-1"
-                               placeholder="Enter password"
-                               value={login.email}
-                               onChange={onChange}
-                        />
-                    </div>
-                    <div className="d-grid gap-2 mt-3">
-                        <button type="submit" className="btn btn-primary">
-                            Submit
-                        </button>
-                    </div>
+        <Formik initialValues={{
+            email: '',
+            password:''
+        }}
+                onSubmit={(login, helper) => {
+
+                    console.log("login", login)
+                }}
+                validate={validate}
+        >
+            {props => (
+                <div className="Auth-form-container">
+                    <Form className="Auth-form" >
+                        <div className="Auth-form-content">
+                            <h3 className="Auth-form-title">Sign In</h3>
+                            <div className="form-group mt-3">
+                                <label>Email address</label>
+                                <input type="email"
+                                       className="form-control mt-1"
+                                       placeholder="Enter email"
+                                       onChange={props.handleChange}
+                                       onBlur={props.handleBlur}
+                                       value={props.values.name}
+                                />
+                                {
+                                    props.errors.email && <span>{props.errors.email}</span>
+                                }
+                            </div>
+                            <div className="form-group mt-3">
+                                <label>Password</label>
+                                <input type="password"
+                                       className="form-control mt-1"
+                                       placeholder="Enter password"
+                                       onChange={props.handleChange}
+                                       onBlur={props.handleBlur}
+                                       value={props.values.name}
+                                />
+                                {
+                                    props.errors.password && <span>{props.errors.password}</span>
+                                }
+                            </div>
+                            <div className="d-grid gap-2 mt-3">
+                                <button type="submit" className="btn btn-primary">
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                    </Form>
                 </div>
-            </form>
-        </div>
+            )}
+        </Formik>
     )
 }
 
